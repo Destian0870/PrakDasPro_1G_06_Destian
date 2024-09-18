3ff35cd1a31a1dd073da919babf96a0e3e370c50
Destian0870 <destian0870@gmail.com>
2024-09-18 18:35:57 +0700

    P3_Percobaan1_PertanyaanNO.5

--- src/Pertemuan3/Siakad06.java
+++ src/Pertemuan3/Siakad06.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 public class Siakad06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            String nama,nim;
-            char kelas;
+            String nama,nim,kelas;
             byte absen;
-            double nilaikuis, nilaitugas, nilaiujian, nilaiakhir;
+            double nilaikuis, nilaitugas, nilaiujian, Nilai-Akhir;
 
             System.out.print("Masukan nama: ");
             nama = sc.nextLine();
             System.out.print("Masukan NIM: ");
             nim = sc.nextLine();
             System.out.print("Masukan Kelas: ");
-            kelas = sc.nextLine().charAt(0);
-            System.out.print("Masukan nomor absen");
+            kelas = sc.nextLine();
+            System.out.print("Masukan nomor absen: ");
             absen = sc.nextByte();
 
             System.out.print("Masukan nilai kuis: ");
@@ -24,11 +23,11 @@ public class Siakad06 {
             nilaitugas = sc.nextDouble();
             System.out.print("Masukan nilai ujian: ");
             nilaiujian = sc.nextDouble();
-            nilaiakhir = (nilaikuis + nilaitugas + nilaiujian) / 3;
+            Nilai-Akhir = (nilaikuis + nilaitugas + nilaiujian) / 3;
 
-            System.out.println("Nama: " + nama + "NIM" + nim);
+            System.out.println("Nama: " + nama + " NIM: " + nim);
             System.out.println("Kelas: " + kelas + " Absen: " + absen);
-            System.out.println("Nilai Akhir: " + nilaiakhir);
+            System.out.println("Nilai Akhir: " + Nilai-Akhir);
 
 
 
