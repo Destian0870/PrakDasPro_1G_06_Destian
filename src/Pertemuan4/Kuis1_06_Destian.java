ec9488f82a0e8667be1dfb53fcd288f24a8e82b3
Destian0870 <destian0870@gmail.com>
2024-09-19 19:52:18 +0700

    Kui1_06_Destian

---
+++ src/Pertemuan4/Kuis1_06_Destian.java
@@ -0,0 +1,28 @@
+package Pertemuan4;
+
+import java.util.Scanner;
+
+public class Kuis1_06_Destian {
+    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Nama    : Destian Dwi Hardika");
+        System.out.println("Nim     : 244107020203");
+        System.out.println("Absen   : 06");
+        System.out.println("==================QUIZ 1 TI - 1G===============");
+
+        int tahunberangkat6, tahunpulang6;
+        System.out.print("Masukan tahunberngkat: ");
+        tahunberangkat6 = input.nextInt();
+        System.out.print("Masukan tahunpulang: ");
+        tahunpulang6 = input.nextInt();
+
+        
+
+
+        
+        
+    }
+
+    
+    
+}
