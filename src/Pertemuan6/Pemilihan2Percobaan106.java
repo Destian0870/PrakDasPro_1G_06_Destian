67c09438c775b0e0cd1dfdb59085cd09a4d0e767
Destian0870 <destian0870@gmail.com>
2024-10-09 22:24:49 +0700

    P6_percoobaan1

---
+++ src/Pertemuan6/Pemilihan2Percobaan106.java
@@ -0,0 +1,23 @@
+package Pertemuan6;
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan106 {
+    public static void main(String[] args) {
+        Scanner input06 = new Scanner (System.in);
+        
+        int tahun;
+
+        System.out.print("Masukan tahun: ");
+        tahun = input06.nextInt();
+
+            if ((tahun % 4) == 0) {
+                if ((tahun % 100) != 0)
+                    System.out.println("Tahun Kabisat");
+            } else
+                System.out.println("Bukan Tahun Kabisat");
+            
+
+        
+    }
+    
+}
