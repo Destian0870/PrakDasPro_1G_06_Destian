7b8f18c37587d6073dddcb7106f95b2729f18643
Destian0870 <destian0870@gmail.com>
2024-10-10 21:38:14 +0700

    P6_Percobaan3

--- src/Pertemuan6/Pemilihan2Perecobaan306.java
+++ src/Pertemuan6/Pemilihan2Perecobaan306.java
@@ -2,5 +2,38 @@ package Pertemuan6;
 import java.util.Scanner;
 
 public class Pemilihan2Perecobaan306 {
+    public static void main(String[] args) {
+        Scanner input06 = new Scanner (System.in);
+
+        String kategori;
+        int penghasilan, gajibersih;
+        double pajak = 0;
+
+        System.out.print("Masukan Kategori : ");
+        kategori = input06.nextLine();
+        System.out.println("Masukan Besarnya Penghasilan : ");
+        penghasilan = input06.nextInt();
+
+        if (kategori.equalsIgnoreCase("pekerja")) {
+            if (penghasilan <= 2000000)
+                pajak = 0.1;
+            else if (penghasilan <= 3000000)
+                pajak = 0.15;
+            else
+                pajak = 0.2;
+            gajibersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan Bersih : " + gajibersih);
+        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+            if (penghasilan <= 2500000)
+                pajak = 0.15;
+            else if (penghasilan <= 3500000)
+                pajak = 0.2;
+            else
+                pajak = 0.25;
+            gajibersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasilan Bersih : " + gajibersih);
+        } else
+            System.out.println("Masukan Kategori Salah ");                                
+    }
     
 }
