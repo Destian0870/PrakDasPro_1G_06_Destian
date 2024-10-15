b52e8d610526eeecd6cde1202be3f887c65e55a1
Destian0870 <destian0870@gmail.com>
2024-10-15 19:22:21 +0700

    P5_Tugas2

---
+++ src/Pertemuan5/kafe06.java
@@ -0,0 +1,38 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class kafe06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        boolean keanggotaan;
+        int jmlKopi, jmlTeh, jmlRoti;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
+        float diskon = 10 / 100;
+
+        System.out.print("Masukkan keangotaan (true/false): ");
+        keanggotaan = input.nextBoolean();
+        System.out.print("Masukan jumlah pembelian kopi: ");
+        jmlKopi = input.nextInt();
+        System.out.print("Masukan jumlah pembelian teh: ");
+        jmlTeh = input.nextInt();
+        System.out.print("Masukan jumlah pembelian roti: ");
+        jmlRoti = input.nextInt();
+
+        totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+        nominalBayar = totalHarga - (diskon * totalHarga);
+        byte totalByte = (byte) totalHarga;
+
+            if (keanggotaan == true) {
+        System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
+        System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar dalam bentuk int Rp " + nominalBayar);
+            } else {
+        int nominalInt = (int) totalHarga;
+        System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
+        System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar : "+totalHarga);
+        }
+    } 
+}
