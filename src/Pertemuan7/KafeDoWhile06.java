fa1d5af99a497188171b193c516fa10c00607aaa
Destian0870 <destian0870@gmail.com>
2024-10-15 17:44:22 +0700

    P7_Percobaan3

---
+++ src/Pertemuan7/KafeDoWhile06.java
@@ -0,0 +1,37 @@
+package Pertemuan7;
+import java.util.Scanner;
+
+public class KafeDoWhile06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int kopi, teh, roti, totalharga, hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+        String namaPelanggan;
+
+        do {
+            System.out.print("Masukan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Tranksaksi dibatalkan.");
+                break;
+            }
+            
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            totalharga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total yang harus dibayar: Rp " + totalharga);
+            sc.nextLine();
+
+        }
+         while (true);
+        
+        System.out.println("Semua tranksaksi selsai.");
+        
+    }
+    
+}
