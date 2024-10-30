2f656c44f182bae61b083fcc3da6ea0b231e0aa5
Destian0870 <destian0870@gmail.com>
2024-10-30 20:47:52 +0700

    P9_Percobaan1

---
+++ src/Pertrmuan9/Star06.java
@@ -0,0 +1,17 @@
+package Pertrmuan9;
+import java.util.Scanner;
+
+public class Star06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.err.print("Masukan nilai N = ");
+        int N = sc.nextInt();
+        
+        for (int i = 1; i<=N; i++) {
+            System.err.print("*");
+            
+        }
+    }
+    
+}
