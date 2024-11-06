3e0ed7c0bff5f05ea1e82435e6b824fbdaa9de0a
Destian0870 <destian0870@gmail.com>
2024-11-06 22:43:31 +0700

    P10_Percobaan1_PertanyaanNo5

--- src/Pertemuan10/ArrayBilangan06.java
+++ src/Pertemuan10/ArrayBilangan06.java
@@ -4,19 +4,12 @@ import java.util.Scanner;
 public class ArrayBilangan06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] bil = {5,13,-7,17};
+        //double[] bil = new double[4];
 
-        int[] bil = new int[4];
-
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-        
+       for (int i = 0; i <=4; i++) {
+            System.out.println(bil[i]);        
+       }
     }
     
 }
