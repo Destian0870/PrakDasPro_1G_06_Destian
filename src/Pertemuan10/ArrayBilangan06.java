db95990be3030434a5d6c5001366531a57dd3618
Destian0870 <destian0870@gmail.com>
2024-11-06 22:54:14 +0700

    P10_Percobaan1_PertanyaanNo5

--- src/Pertemuan10/ArrayBilangan06.java
+++ src/Pertemuan10/ArrayBilangan06.java
@@ -5,7 +5,7 @@ public class ArrayBilangan06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] bil = {5,13,-7,17};
-        //double[] bil = new double[4];
+       
 
        for (int i = 0; i <=4; i++) {
             System.out.println(bil[i]);        
