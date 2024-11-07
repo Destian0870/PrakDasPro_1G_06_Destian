75e89698eb58bed3fe4bacc56f410d8b067a8794
Destian0870 <destian0870@gmail.com>
2024-11-07 19:25:52 +0700

    P10_Percobaan2

---
+++ src/Pertemuan10/ArrayNilai06.java
@@ -0,0 +1,18 @@
+package Pertemuan10;
+import java.util.Scanner;
+
+public class ArrayNilai06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Masukan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+             for (int i = 0; i < 10; i++) {
+                 System.out.println("nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+             }
+
+    } 
+}
