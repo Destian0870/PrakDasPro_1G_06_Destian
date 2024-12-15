9987bf887bb90baa4673f634f6dfd922626d53b9
Destian0870 <destian0870@gmail.com>
2024-12-15 18:42:34 +0700

    P13_Percobaan1

---
+++ src/Pertemuan13/Kafe06.java
@@ -0,0 +1,19 @@
+package Pertemuan13;
+import java.util.Scanner;
+
+public class Kafe06 {
+    public static void Menu() {
+     System.out.println("===== MENU RESTO KAFE =====");
+     System.out.println("1. kopi hitam - Rp15,000");
+     System.out.println("2. Cappuccino - Rp20,000");
+     System.out.println("3. Latte - Rp 22,000");
+     System.out.println("4. Teh Tarik - Rp 12,000");
+     System.out.println("5. Roti Bakar - Rp 10,000");
+     System.out.println("6. Mie Goreng - Rp 18,000");
+     System.out.println("===========================");
+     System.out.println("Silahkan pilih menu yang anda inginkan");
+    }     
+    public static void main(String[] args) {
+        Menu();
+     }   
+}
