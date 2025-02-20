6edf573c27e087ce8b47dbc4a450dba3da9d8cc7
Destian0870 <destian0870@gmail.com>
2025-02-20 10:30:34 +0700

    Percobaan 4

--- src/Pertemuan2_SEmester2/MahasiswaMain06.java
+++ src/Pertemuan2_SEmester2/MahasiswaMain06.java
@@ -1,30 +1,7 @@
 package Pertemuan2_SEmester2;
-
-class Mahasiswa {
-    String nama;
-    String nim;
-    String kelas;
-    double ipk;
-
-    void tampilkanInformasi() {
-        System.out.println("Nama: " + nama);
-        System.out.println("NIM: " + nim);
-        System.out.println("IPK: " + ipk);
-        System.out.println("Kelas: " + kelas);
-    }
-
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIpk(double ipkBaru) {
-        ipk = ipkBaru;
-    }
-}
-
 public class MahasiswaMain06{
     public static void main(String[] args) {
-        Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa06 mhs1 = new Mahasiswa06();
         
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
@@ -37,5 +14,9 @@ public class MahasiswaMain06{
         mhs1.updateIpk(3.60);
 
         mhs1.tampilkanInformasi();
+
+        Mahasiswa06 mhs2 = new Mahasiswa06("Annisa Nabila","2141720160", 3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
